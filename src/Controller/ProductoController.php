6a01bdbdcdf59519e5b50eb66695f63885d3fa75
Daniel Castro <dcaste@uc.cl>
2025-08-29 13:12:30 -0400

    Formularios.

--- src/Controller/ProductoController.php
+++ src/Controller/ProductoController.php
@@ -6,25 +6,70 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductoRepository;
+use App\Entity\Producto;
+use App\Form\ProductoType;
 
 final class ProductoController extends AbstractController
 {
-    #[Route('/productos', name: 'producto_index')]
-    // Asegurarse de agregar el repository como argumento del método
+	#[Route('/productos', name: 'producto_index')]
+	// Asegurarse de agregar el repository como argumento del método
 	public function index(ProductoRepository $productoRepository): Response
-    {
+	{
 		// retreive products from the database if needed
 		$products = $productoRepository->findAll();
-		
+
 		// dd($products); // dump and die
 
 		echo '<pre><h1>Lista de Productos</h1>';
 		// print_r($products); // print and continue
 		echo '</pre>';
 
-        return $this->render('producto/index.html.twig', [
-            'controller_name' => 'ProductoController',
+		return $this->render('producto/index.html.twig', [
+			'controller_name' => 'ProductoController',
 			'products' => $products,
-        ]);
-    }
+		]);
+	}
+
+
+	// DEPRECATED: la opcion siguiente es mejor
+	// el <> es para restringir el tipo de parametro (aquí, solo números)
+	#[Route('/producto_old/{id<\d+>}', name: 'producto_show')]
+	public function show(int $id, ProductoRepository $productoRepository): Response
+	{
+		// Distintos metodos:
+		//SOlo por id
+		// $product = $productoRepository->find($id);
+		//multiples columnas
+		$product = $productoRepository->findOneBy(['id' => $id]);
+		dd($product);
+		if (!$product) {
+			throw $this->createNotFoundException('El producto con id ' . $id . ' no existe');
+		}
+		return $this->render('producto/show.html.twig', [
+			'product' => $product,
+		]);
+	}
+
+	// opcion mejor
+	// el <> es para restringir el tipo de parametro (aquí, solo números)
+	#[Route('/producto/{id<\d+>}', name: 'producto_mostrar')]
+	public function mostrar(Producto $producto): Response
+	{
+		// El parametro $producto es inyectado automaticamente por Symfony
+		// Si no se encuentra, lanza una excepcion 404 automaticamente
+		return $this->render('producto/show.html.twig', [
+			'product' => $producto,
+		]);
+	}
+
+	#[Route('/producto/nuevo', name: 'producto_nuevo')]
+	public function new(): Response
+	{
+		$form = $this->createForm(ProductoType::class);
+		return $this->render('producto/new.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
+
+
 }
---
+++ src/Form/ProductoType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Producto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType; // Agregado a mano
+
+class ProductoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('descripcion')
+            ->add('size')
+            ->add('precio')
+			->add('save', SubmitType::class, ['label' => 'Crear Producto']) // Botón de envío
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Producto::class,
+        ]);
+    }
+}
